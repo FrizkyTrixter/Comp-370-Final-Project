57b4807ce057cab0a6a9397eb669fbfaf410e7cb
Efe Ertugrul <efertugrul6@gmail.com>
2024-11-21 12:47:40 -0500

    Update tf_idf.py

--- scripts/tf_idf.py
+++ scripts/tf_idf.py
@@ -5,10 +5,8 @@ from collections import defaultdict
 
 
 df = pd.read_csv('../data/final/merged_annotated_articles.tsv', sep='\t')
-
 with open('stopwords.txt', 'r') as f:
     stopwords = set(f.read().split())
-
 df['Text'] = df['Title'].fillna('') + ' ' + df['Description'].fillna('') + ' ' + df['Content'].fillna('')
 
 def preprocess(text):
@@ -17,23 +15,22 @@ def preprocess(text):
     words = text.split()
     words = [word for word in words if word not in stopwords]
     return words
-
 df['Tokens'] = df['Text'].apply(preprocess)
-vocab = set()
+words = set()
 document_frequencies = defaultdict(int)
 total_documents = len(df)
-
 for tokens in df['Tokens']:
     unique_tokens = set(tokens)
-    vocab.update(unique_tokens)
+    words.update(unique_tokens)
     for token in unique_tokens:
         document_frequencies[token] += 1
-
 idf = {}
 for term, df_t in document_frequencies.items():
     idf[term] = math.log(total_documents / (1 + df_t)) 
 
 df['TF_IDF'] = df['Tokens'].apply(lambda tokens: {term: tokens.count(term) * idf[term] for term in tokens})
+
+
 
 
 
